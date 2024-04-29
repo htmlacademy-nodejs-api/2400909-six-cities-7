7a22b0bc09f1ea7440661a26af1191e368c403c5
Дмитрий Остапенко <nugashop@yandex.ru>
2024-04-29 21:30:40 +0600

    add: chalk

--- src/cli/commands/help.command.ts
+++ src/cli/commands/help.command.ts
@@ -1,3 +1,4 @@
+import chalk from 'chalk';
 import { Command } from './command.interface.js';
 import { CommandType } from './const.js';
 
@@ -10,13 +11,15 @@ export class HelpCommand implements Command {
   public async execute(..._parameters: string[]): Promise<void> {
     console.info(`
         Программа для подготовки данных для REST API сервера.
+
         Пример:
-            cli.js --<command> [--arguments]
+            ${chalk.bgCyan('cli.js --<command> [--arguments]')}
+
         Команды:
-            --version:                   # выводит номер версии
-            --help:                      # печатает этот текст
-            --import <path>:             # импортирует данные из TSV
-            --generate <n> <path> <url>  # генерирует произвольное количество тестовых данных
+            ${chalk.blue('--version')}:                   ${chalk.green('# выводит номер версии')}
+            ${chalk.blue('--help')}:                      ${chalk.green('# печатает этот текст')}
+            ${chalk.blue('--import')} <path>:             ${chalk.green('# импортирует данные из TSV')}
+            ${chalk.blue('--generate')} <n> <path> <url>  ${chalk.green('# генерирует произвольное количество тестовых данных')}
     `);
   }
 }
