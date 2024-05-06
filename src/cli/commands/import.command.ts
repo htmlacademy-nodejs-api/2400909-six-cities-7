3634cba6df4832a62ea9b9a35cfddd38267250cc
Дмитрий Остапенко <nugashop@yandex.ru>
2024-05-06 22:56:51 +0600

    Рефакторит команду ImportCommand

--- src/cli/commands/import.command.ts
+++ src/cli/commands/import.command.ts
@@ -1,28 +1,34 @@
+import { getErrorMessage } from '../../shared/helpers/common.js';
 import { TSVFileReader } from '../../shared/libs/file-reader/tsv-file-reader.js';
+import { Offer } from '../../shared/types/offer.type.js';
 import { Command } from './command.interface.js';
 import { CommandType } from './const.js';
 
 export class ImportCommand implements Command {
+  private onImportedOffer(offer: Offer): void {
+    console.info(offer);
+  }
+
+  private onCompletedImport(count: number) {
+    console.info(`${count} rows imported.`);
+  }
+
   public getName(): string {
     return CommandType.Import;
   }
 
-  public execute(...parameters: string[]): void {
-    // Чтение файла
+  public async execute(...parameters: string[]): Promise<void> {
     const [filename] = parameters;
     const fileReader = new TSVFileReader(filename.trim());
 
+    fileReader.on('read', this.onImportedOffer);
+    fileReader.on('end', this.onCompletedImport);
+
     try {
       fileReader.read();
-      console.log(fileReader.toArray());
-    } catch (err) {
-
-      if (!(err instanceof Error)) {
-        throw err;
-      }
-
+    } catch (error) {
       console.error(`Can't import data from file: ${filename}`);
-      console.error(`Details: ${err.message}`);
+      console.error(getErrorMessage(error));
     }
   }
 }
