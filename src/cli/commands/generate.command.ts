40bacbaae46691adf9de0df63fa2eec05c59bd98
Дмитрий Остапенко <nugashop@yandex.ru>
2024-05-06 14:06:27 +0600

    Добавит пакет got

--- src/cli/commands/generate.command.ts
+++ src/cli/commands/generate.command.ts
@@ -1,14 +1,14 @@
-import { Command } from "./command.interface.js";
-import { CommandType } from "./const.js";
+import { Command } from './command.interface.js';
+import { CommandType } from './const.js';
 
 export class GenerateCommand implements Command {
-  public public getName(): string {
+  public getName(): string {
     return CommandType.Generate;
   }
 
-  public execute(...parameters: string[]): void {
-    const [count, filepath, url] = parameters;
-    const offerCount = Number.parseInt(count, 10);
+  public execute(..._parameters: string[]): void {
+    // const [count, filepath, url] = parameters;
+    // const offerCount = Number.parseInt(count, 10);
 
     // Код для получения данных с сервера.
     // Формирование объявлений.
