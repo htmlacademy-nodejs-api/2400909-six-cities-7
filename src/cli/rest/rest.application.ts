611452cfb162c97fb4ac6df774ea01adf0911ed1
Дмитрий Остапенко <nugashop@yandex.ru>
2024-05-14 22:05:02 +0600

    Внедрит зависимости в RestApplication

--- src/cli/rest/rest.application.ts
+++ src/cli/rest/rest.application.ts
@@ -1,10 +1,15 @@
+import { inject, injectable } from 'inversify';
+
 import { Config } from '../../shared/libs/config/config.interface.js';
 import { Logger } from '../../shared/libs/logger/logger.interface.js';
+import { Component } from '../../shared/types/component.enum.js';
+import { RestSchema } from '../../shared/libs/config/rest.schema.js';
 
+@injectable()
 export class RestApplication {
   constructor(
-    private readonly logger: Logger,
-    private readonly config: Config,
+    @inject(Component.Logger) private readonly logger: Logger,
+    @inject(Component.Config) private readonly config: Config<RestSchema>,
   ) {}
 
   public async init() {
