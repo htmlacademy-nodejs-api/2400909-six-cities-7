2870c62d4444f8843e3fbfa60b42e51b037dc796
Дмитрий Остапенко <nugashop@yandex.ru>
2024-05-25 18:09:21 +0600

    Добавит определение модели и схемы.

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,9 +1,22 @@
+import { getModelForClass, prop } from "@typegoose/typegoose";
+
 import { UserData } from "../../types/user-data.type.js";
 
 export class UserEntity implements UserData {
+  @prop({required: true})
   public name: string;
+
+  @prop({unique: true, required: true})
   public email: string;
+
+  @prop({unique: true, required: true})
   public avatarUrl: string;
+
+  @prop({required: true})
   public password: string;
+
+  @prop({required: true})
   public isPro: boolean;
 }
+
+export const UserModel = getModelForClass(UserEntity);
