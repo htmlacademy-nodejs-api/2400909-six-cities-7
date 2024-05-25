ed04efd39d31485b958009d1ba2e20e10aecbeeb
Дмитрий Остапенко <nugashop@yandex.ru>
2024-05-25 17:37:44 +0600

    Добавит уникальность для поля email

--- src/shared/modules/user/user.model.ts
+++ src/shared/modules/user/user.model.ts
@@ -7,7 +7,10 @@ export interface UserDocument extends UserData, Document {
 }
 
 const userSchema = new Schema({
-  email: String,
+  email: {
+    type: String,
+    unique: true,
+  },
   avatarPath: String,
   firstname: String,
   lastname: String,
