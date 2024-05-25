eed658c896bf83d85eafb40ac2fec52cbc1f0e78
Дмитрий Остапенко <nugashop@yandex.ru>
2024-05-25 18:32:27 +0600

    Добавит методы в UserEntity

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,13 +1,21 @@
-import { defaultClasses, getModelForClass, prop } from "@typegoose/typegoose";
+import { defaultClasses, getModelForClass, modelOptions, prop } from '@typegoose/typegoose';
 
-import { UserData } from "../../types/user-data.type.js";
+import { UserData } from '../../types/user-data.type.js';
+import { createSHA256 } from '../../helpers/hash.js';
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export interface UserEntity extends defaultClasses.Base {}
 
+@modelOptions({
+  schemaOptions: {
+    collection: 'users',
+    timestamps: true,
+  }
+})
+
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export class UserEntity extends defaultClasses.TimeStamps implements UserData {
-  @prop({required: true})
+  @prop({required: true, default: ''})
   public name: string;
 
   @prop({unique: true, required: true})
@@ -16,11 +24,29 @@ export class UserEntity extends defaultClasses.TimeStamps implements UserData {
   @prop({unique: true, required: false, default: ''})
   public avatarUrl: string;
 
-  @prop({required: true})
+  @prop({required: true, default: ''})
   public password: string;
 
   @prop({required: true})
   public isPro: boolean;
+
+  constructor(userData: UserData) {
+    super();
+
+    this.name = userData.name;
+    this.email = userData.email;
+    this.avatarUrl = userData.avatarUrl;
+    this.password = userData.password;
+    this.isPro = userData.isPro;
+  }
+
+  public setPassword(password: string, salt: string) {
+    this.password = createSHA256(password, salt);
+  }
+
+  public getPassword() {
+    return this.password;
+  }
 }
 
 export const UserModel = getModelForClass(UserEntity);
