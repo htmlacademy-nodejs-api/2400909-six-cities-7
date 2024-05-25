774254348c13724e315ec062767cfa9701f482a3
Дмитрий Остапенко <nugashop@yandex.ru>
2024-05-25 17:35:48 +0600

    Расширит интерфейс UserDocument

--- src/shared/modules/user/user.model.ts
+++ src/shared/modules/user/user.model.ts
@@ -1,13 +1,16 @@
 import { Schema, Document, model } from 'mongoose';
 import { UserData } from '../../types/user-data.type.js';
 
-export interface UserDocument extends UserData, Document {}
+export interface UserDocument extends UserData, Document {
+  createAt: Date,
+  updateAt: Date,
+}
 
 const userSchema = new Schema({
   email: String,
   avatarPath: String,
   firstname: String,
   lastname: String,
-});
+}, {timestamps: true});
 
 export const UserModel = model<UserDocument>('User', userSchema);
