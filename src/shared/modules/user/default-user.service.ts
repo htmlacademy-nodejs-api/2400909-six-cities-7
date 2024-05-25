2285269b3945dc65fcb66b863c0963994a448b05
Дмитрий Остапенко <nugashop@yandex.ru>
2024-05-25 19:05:40 +0600

    Разобьёт общий контейнер

--- src/shared/modules/user/default-user.service.ts
+++ src/shared/modules/user/default-user.service.ts
@@ -1,7 +1,7 @@
-import { DocumentType } from "@typegoose/typegoose";
-import { CreateUserDto } from "./dto/create-user.dto.js";
-import { UserService } from "./user-service.interface.js";
-import { UserEntity, UserModel } from "./user.entity.js";
+import { DocumentType } from '@typegoose/typegoose';
+import { CreateUserDto } from './dto/create-user.dto.js';
+import { UserService } from './user-service.interface.js';
+import { UserEntity, UserModel } from './user.entity.js';
 
 export class DefaultUserService implements UserService {
   public async create(dto: CreateUserDto, salt: string): Promise<DocumentType<UserEntity>> {
