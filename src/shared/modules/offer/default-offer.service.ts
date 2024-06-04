22bac6419e29e0eb284460bff27912f6846c7c22
DmitriiOst <nugashop@yandex.ru>
2024-06-04 13:42:23 +0300

    Реализует обновлённый интерфейс OfferService

--- src/shared/modules/offer/default-offer.service.ts
+++ src/shared/modules/offer/default-offer.service.ts
@@ -5,6 +5,8 @@ import { Logger } from '../../libs/logger/logger.interface.js';
 import { DocumentType, types } from '@typegoose/typegoose';
 import { OfferEntity } from './offer.entity.js';
 import { CreateOfferDto } from './dto/create-offer.dto.js';
+import { UpdateOfferDto } from './dto/update-offer.dto.js';
+import { SortType } from '../../types/sort-type.enum.js';
 
 @injectable()
 export class DefaultOfferService implements OfferService {
@@ -28,6 +30,52 @@ export class DefaultOfferService implements OfferService {
   }
 
   public async find(): Promise<DocumentType<OfferEntity>[]> {
-    return this.offerModel.find();
+    return this.offerModel
+      .find()
+      .populate(['userId', 'offers'])
+      .exec();
+  }
+
+  public async deleteById(offerId: string): Promise<DocumentType<OfferEntity> | null> {
+    return this.offerModel
+      .findByIdAndDelete(offerId)
+      .exec();
+  }
+
+  public async updateById(offerId: string, dto: UpdateOfferDto): Promise<DocumentType<OfferEntity> | null> {
+    return this.offerModel
+      .findByIdAndUpdate(offerId, dto, {new: true})
+      .populate(['userId', 'offers'])
+      .exec();
+  }
+
+  public async exists(documentId: string): Promise<boolean> {
+    return (await this.offerModel
+      .exists({_id: documentId})) !== null;
+  }
+
+  public async incCommentCount(offerId: string): Promise<DocumentType<OfferEntity> | null> {
+    return this.offerModel
+      .findByIdAndUpdate(offerId, {'$inc': {
+        commentCount: 1,
+      }}).exec();
+  }
+
+  public async findNew(count: number): Promise<DocumentType<OfferEntity>[]> {
+    return this.offerModel
+      .find()
+      .sort({createAt: SortType.Down})
+      .limit(count)
+      .populate(['userId', 'offers'])
+      .exec();
+  }
+
+  public async findDiscussed(count: number): Promise<DocumentType<OfferEntity>[]> {
+    return this.offerModel
+      .find()
+      .sort({commentCount: SortType.Down})
+      .limit(count)
+      .populate(['userId', 'offers'])
+      .exec();
   }
 }
