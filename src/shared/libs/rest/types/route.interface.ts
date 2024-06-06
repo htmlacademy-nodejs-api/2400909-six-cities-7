cd60cee7a5535e903aa2f5221158e906658f44f1
DmitriiOst <nugashop@yandex.ru>
2024-06-06 10:44:49 +0300

    Добавит интерфейс Controller

--- src/shared/libs/rest/types/route.interface.ts
+++ src/shared/libs/rest/types/route.interface.ts
@@ -1,5 +1,5 @@
-import { NextFunction, Request, Response } from "express";
-import { HttpMethod } from "./http-method.enum.js";
+import { NextFunction, Request, Response } from 'express';
+import { HttpMethod } from './http-method.enum.js';
 
 export interface Route {
   path: string;
