adbb3e22dd7894e9f2363cf1c25c9f1b2a2980d6
DmitriiOst <nugashop@yandex.ru>
2024-06-06 15:12:23 +0300

    Добавим обработку HttpError в AppExceptionFilter

--- src/shared/libs/rest/exception-filter/app-exception-filter.ts
+++ src/shared/libs/rest/exception-filter/app-exception-filter.ts
@@ -4,6 +4,8 @@ import { Component } from '../../../types/component.enum.js';
 import { Logger } from '../../logger/logger.interface.js';
 import { Request, Response, NextFunction } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import { HttpError } from '../errors/http-error.js';
+import { createErrorObject } from '../../../helpers/common.js';
 
 @injectable()
 export class AppExceptionFilter implements ExceptionFilter {
@@ -13,10 +15,25 @@ export class AppExceptionFilter implements ExceptionFilter {
     this.logger.info('Register AppExceptionFilter');
   }
 
-  public catch(error: Error, _req: Request, res: Response, _next: NextFunction): void {
+  private handleHttpError(error: HttpError, _req: Request, res: Response, _next: NextFunction) {
+    this.logger.error(`[${error.detail}]: ${error.httpStatusCode} - ${error.message}`, error);
+    res
+      .status(error.httpStatusCode)
+      .json(createErrorObject(error.message));
+  }
+
+  private handleOtherError(error: Error, _req: Request, res: Response, _next: NextFunction) {
     this.logger.error(error.message, error);
     res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({error: error.message});
+      .json(createErrorObject(error.message));
+  }
+
+  public catch(error: Error | HttpError, req: Request, res: Response, next: NextFunction): void {
+    if (error instanceof HttpError) {
+      return this.handleHttpError(error, req, res, next);
+    }
+
+    this.handleOtherError(error, req, res, next);
   }
 }
