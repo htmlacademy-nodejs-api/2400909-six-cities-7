425b07fc5dee6244a4a50ab3b4f13f3f450ac6a1
DmitriiOst <nugashop@yandex.ru>
2024-06-09 21:00:36 +0300

    Подключит middleware для проверки ObjectId во все контроллеры.

--- src/shared/libs/rest/middleware/middleware.interface.ts
+++ src/shared/libs/rest/middleware/middleware.interface.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from 'express';
 
 export interface Middleware {
   execute(req: Request, res: Response, next: NextFunction): void;
--- src/shared/libs/rest/middleware/validate-objectid.middleware.ts
+++ src/shared/libs/rest/middleware/validate-objectid.middleware.ts
@@ -1,9 +1,9 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from 'express';
 
-import { Middleware } from "./middleware.interface.js";
-import { Types } from "mongoose";
-import { HttpError } from "../errors/http-error.js";
-import { StatusCodes } from "http-status-codes";
+import { Middleware } from './middleware.interface.js';
+import { Types } from 'mongoose';
+import { HttpError } from '../errors/http-error.js';
+import { StatusCodes } from 'http-status-codes';
 
 export class ValidateObjectIdMiddleware implements Middleware {
   constructor(private param: string) {}
