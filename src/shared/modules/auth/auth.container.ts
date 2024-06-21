36e5fce1a338711ea6bfd8ab41d343d37e493a5b
DmitriiOst <nugashop@yandex.ru>
2024-06-21 16:37:33 +0300

    Добавит login в UserController

--- src/shared/modules/auth/auth.container.ts
+++ src/shared/modules/auth/auth.container.ts
@@ -1,10 +1,10 @@
-import { Container } from "inversify";
+import { Container } from 'inversify';
 
-import { AuthService } from "./auth-service.interface.js";
-import { Component } from "../../types/component.enum.js";
-import { DefaultAuthService } from "./default-auth.service.js";
-import { ExceptionFilter } from "../../libs/rest/exception-filter/exception-filter.interface.js";
-import { AuthExceptionFilter } from "./auth.exception-filter.js";
+import { AuthService } from './auth-service.interface.js';
+import { Component } from '../../types/component.enum.js';
+import { DefaultAuthService } from './default-auth.service.js';
+import { ExceptionFilter } from '../../libs/rest/exception-filter/exception-filter.interface.js';
+import { AuthExceptionFilter } from './auth.exception-filter.js';
 
 export function createAuthContainer() {
   const authContainer = new Container();
--- src/shared/modules/user/user.controller.ts
+++ src/shared/modules/user/user.controller.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from 'inversify';
 import { Response, Request } from 'express';
+
 import { BaseController } from '../../libs/rest/controller/base-controller.abstract.js';
 import { Component } from '../../types/component.enum.js';
 import { Logger } from '../../libs/logger/logger.interface.js';
@@ -18,6 +19,8 @@ import { CreateUserDto } from './dto/create-user.dto.js';
 import { LoginUserDto } from './dto/login-user.dto.js';
 import { ValidateObjectIdMiddleware } from '../../libs/rest/middleware/validate-objectid.middleware.js';
 import { UploadFileMiddleware } from '../../libs/rest/middleware/upload-file.middleware.js';
+import { AuthService } from '../auth/auth-service.interface.js';
+import { LoggedUserRdo } from './rdo/logger-user.rdo.js';
 
 @injectable()
 export class UserController extends BaseController {
@@ -25,6 +28,7 @@ export class UserController extends BaseController {
     @inject(Component.Logger) protected readonly logger: Logger,
     @inject(Component.UserService) protected readonly userService: UserService,
     @inject(Component.Config) protected readonly configService: Config<RestSchema>,
+    @inject(Component.AuthService) protected readonly authService: AuthService,
   ) {
     super(logger);
     this.logger.info('Register routes for UserController…');
@@ -72,23 +76,15 @@ export class UserController extends BaseController {
 
   public async login(
     { body }: LoginUserRequest,
-    _res: Response,
+    res: Response,
   ): Promise<void> {
-    const existsUser = await this.userService.findByEmail(body.email);
-
-    if (!existsUser) {
-      throw new HttpError(
-        StatusCodes.UNAUTHORIZED,
-        `User with email ${body.email} not found.`,
-        'UserController',
-      );
-    }
-
-    throw new HttpError(
-      StatusCodes.NOT_IMPLEMENTED,
-      'Not implemented',
-      'UserController',
-    );
+    const user = await this.authService.verify(body);
+    const token = await this.authService.authenticate(user);
+    const responseData = fillDTO(LoggedUserRdo, {
+      email: user.email,
+      token,
+    });
+    this.ok(res, responseData);
   }
 
   public async uploadAvatar(req: Request, res: Response) {
